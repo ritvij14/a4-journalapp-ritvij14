c588155089af991e4e46213e5188c0cc69df7341
ritvij14 <ritvij2001@gmail.com>
2022-11-26 02:02:51 +0530

    :art: added readme and instrumented test cases

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ BITS ID: 2019A8PS0666G
 
 Email: f20190666@goa.bits-pilani.ac.in
 
+## What does the app do?
+
+It stores journal entries along with start and end times and the date. These entries can be deleted, shared and edited.
+
+## Process of making this app
+
+1. Instead of adding code, I used the user interface to implement the navigation graphs and the actions. They can be referred to repeatedly using the same technique. Instead of utilising the nav graphs to transmit anything, I used sharedViewModel to pass the one bit of information I needed to pass ( StartTime Pressed or EndTime Pressed )
+
+2. I added a delete option and a warning dialogue asking, "Are you sure?" to the menu.xml file.
+
+3. For sharing I used Intent. I set the type of the intent as "text/*" which means it can handle texts and hyperlinks and JSON.
+
+4. Created a simple info fragment and added it into the nav graph
+
+## Testing
+
+I manually tested all parts of the app along with some test cases
+
+## Completion time
+
+21 hours
+
+## Difficulty
+
+9/10
--- app/src/androidTest/java/androidsamples/java/journalapp/EntryDetailsFragmentTest.java
+++ app/src/androidTest/java/androidsamples/java/journalapp/EntryDetailsFragmentTest.java
@@ -1,48 +1,114 @@
 package androidsamples.java.journalapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
 
-import java.util.Objects;
+import android.view.View;
 
 import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.navigation.Navigation;
 import androidx.navigation.testing.TestNavHostController;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.accessibility.AccessibilityChecks;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import static androidx.test.espresso.Espresso.onView;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Objects;
 
 /**
  * Instrumented tests for the {@link EntryDetailsFragment}.
  */
 @RunWith(AndroidJUnit4.class)
 public class EntryDetailsFragmentTest {
-  @Test
-  public void testNavigationToEntryListFragment() {
-    // Create a TestNavHostController
-    TestNavHostController navController = new TestNavHostController(
-        ApplicationProvider.getApplicationContext());
 
-    FragmentScenario<EntryListFragment> entryDetailsFragmentFragmentScenario
-        = FragmentScenario.launchInContainer(EntryListFragment.class, null, R.style.Theme_JournalApp, (FragmentFactory) null);
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    entryDetailsFragmentFragmentScenario.onFragment(fragment -> {
-      // Set the graph on the TestNavHostController
-      navController.setGraph(R.navigation.nav_graph);
 
-      // Make the NavController available via the findNavController() APIs
-      Navigation.setViewNavController(fragment.requireView(), navController);
-    });
+    @BeforeClass
+    public static void enableAccessibilityChecks() {
+        AccessibilityChecks.enable();
+    }
 
-    // Verify that performing a click changes the NavController's state
-    onView(ViewMatchers.withId(R.id.btn_add_entry)).perform(ViewActions.click());
-    assertThat(Objects.requireNonNull(navController.getCurrentDestination()).getId(), is(R.id.entryDetailsFragment));
 
-  }
+    @Test
+    public void testNavigationToEntryListFragment() {
+        // Create a TestNavHostController
+        TestNavHostController navController = new TestNavHostController(
+                ApplicationProvider.getApplicationContext());
+
+        FragmentScenario<EntryListFragment> entryDetailsFragmentFragmentScenario
+                = FragmentScenario.launchInContainer(EntryListFragment.class, null, R.style.Theme_JournalApp, (FragmentFactory) null);
+
+        entryDetailsFragmentFragmentScenario.onFragment(fragment -> {
+            // Set the graph on the TestNavHostController
+            navController.setGraph(R.navigation.nav_graph);
+
+            // Make the NavController available via the findNavController() APIs
+            Navigation.setViewNavController(fragment.requireView(), navController);
+        });
+
+        // Verify that performing a click changes the NavController's state
+        onView(withId(R.id.btn_add_entry)).perform(click());
+        assertThat(Objects.requireNonNull(navController.getCurrentDestination()).getId(), is(R.id.entryDetailsFragment));
+
+    }
+
+    @Test
+    public void testDeletion() {
+        onView(withId(R.id.btn_add_entry)).perform(click());
+        onView(withId(R.id.edit_title)).perform(clearText()).perform(typeText("Hello"));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(anyOf(withText("Hello"))).perform(click());
+        onView(withId(R.id.menu_delete_entry)).perform(click());
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.recycler_view)).check(new RecyclerViewItemCountAssertion(0));
+    }
+
+    @Test
+    public void testInsertion() {
+        onView(withId(R.id.btn_add_entry)).perform(click());
+        onView(withId(R.id.edit_title)).perform(clearText()).perform(typeText("Testing"));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.recycler_view)).check(new RecyclerViewItemCountAssertion(0));
+        onView(anyOf(withText("Testing"))).perform(click());
+        onView(withId(R.id.menu_delete_entry)).perform(click());
+        onView(withText("OK")).perform(click());
+    }
+
+    public static class RecyclerViewItemCountAssertion implements ViewAssertion {
+        private final int expectedCount;
+
+        public RecyclerViewItemCountAssertion(int expectedCount) {
+            this.expectedCount = expectedCount;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException;
+            }
+
+            RecyclerView recyclerView = (RecyclerView) view;
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            assert adapter != null;
+            assertThat(adapter.getItemCount(), is(expectedCount));
+        }
+    }
 }
